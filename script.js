5c7f90ce7cdb368b2842b2f84be27dac72aa0b0e
alee7 <alee7@bu.edu>
2026-02-22 13:46:53 -0500

    Uploaded final

--- script.js
+++ script.js
@@ -19,33 +19,52 @@ if (SpeechRecognition) {
 
   //==============================================================
   //✅ Define keywords and what happens when they are spoken
+
+    // (Welcome) Tiri Kananuruk to a (guest lecture) on (February 27), at (4:30pm). 
+    // Meet her at (room 410) at (808) commonwealth avenue or on (zoom). (See you then!)
+  
   //==============================================================
     const keywords = {
       "Welcome": () => {
         document.querySelector("#mainText").className = "variable0";
-        document.querySelector("#image").src = "./img/P1a.png"
-        document.body.style.backgroundColor = "black";
+        document.querySelector("#image").src = "./img/A1.png"
+        document.body.style.backgroundColor = rgb(115, 63, 28);
       },
-      "lecture": () => {
+      "guest lecture": () => {
         document.querySelector("#mainText").className = "variable1";
-        document.querySelector("#image").src = "./img/P1b-1.png"
-        document.body.style.backgroundColor = "white";
-        document.querySelector("#mainText").textContent = "Tiri Kananuruk";
+        document.querySelector("#image").src = "./img/A2.png"
+        document.body.style.backgroundColor = "tan";
       },
       "February 27": () => {
         document.querySelector("#mainText").className = "variable2";
-        document.querySelector("#image").src = "./img/P1c-1.png"
-        document.body.style.backgroundColor = "white";
+        document.querySelector("#image").src = "./img/A3.png"
+        document.body.style.backgroundColor = "beige";
+      },
+      "4:30": () =>{
+        document.querySelector("#mainText").className = "variable3";
+        document.querySelector("#image").src = "./img/A4.png"
+        document.body.style.backgroundColor = rgb(115, 63, 28);
+      },
+      "room 410": () =>{
+        document.querySelector("#mainText").className = "variable4";
+        document.querySelector("#image").src = "./img/A5.png"
+        document.body.style.backgroundColor = "tan";
       },
       "808": () =>{
-        document.querySelector("#mainText").className = "variable3";
-        document.querySelector("#image").src = "./img/P1d-1.png"
-        document.body.style.backgroundColor = "white";
+        document.querySelector("#mainText").className = "variable5";
+        document.querySelector("#image").src = "./img/A6.png"
+        document.body.style.backgroundColor = "beige";
+      },
+      "on Zoom": () =>{
+        document.querySelector("#mainText").className = "variable6";
+        document.querySelector("#image").src = "./img/A7.png"
+        document.body.style.backgroundColor = "tan";
       },
       "See you then": () =>{
-        document.querySelector("#mainText").className = "variable4";
-        document.querySelector("#image").src = "./img/P1d-2.png"
-        document.body.style.backgroundColor = "white";
+        document.querySelector("#mainText").className = "variable7";
+        document.querySelector("#image").src = "./img/D.gif"
+        document.querySelector("#mainText").textContent = "Tiri Kananuruk";
+        document.body.style.backgroundColor = "tan";
       },
     };
 
